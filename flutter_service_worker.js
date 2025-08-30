7fdfb9278fa0f2b602585adfea3e968af9e2100c
ajmerashreyansh52 <ajmerashreyansh52@users.noreply.github.com>
2025-08-30 10:17:07 +0000

    deploy: 24fd56ffbb4b8566dc59bb37d5231f2f249675ee

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin": "56ffd2a9139af82e4dc836148e95893b",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"flutter_bootstrap.js": "f1b8c71bf38b3ccf468885662eae7426",
+"flutter_bootstrap.js": "4e976ef4642bd4fef452a17478fc9e0f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "2e6695cbfd1e1d39cf94fe24bdaec192",
 "/": "2e6695cbfd1e1d39cf94fe24bdaec192"};
